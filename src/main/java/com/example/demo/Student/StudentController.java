9d4f5259e9c5d72d43ef75c8392ab6b26e7c8099
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2023-11-22 21:56:57 +0000

    Update

--- src/main/java/com/example/demo/Student/StudentController.java
+++ src/main/java/com/example/demo/Student/StudentController.java
@@ -2,9 +2,7 @@ package com.example.demo.Student;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,10 @@ public class StudentController {
         return studentService.getStudents();
     }
 
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
 
 
 }
